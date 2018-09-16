cd1fb7068ce321036210d12dfda2d479f81ea441
Carlos Andrade <carlos.andrade86@outlook.com>
2018-09-16 12:01:01 -0400

    connected authController to server and confirmed it's functional using postman

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const mongoose = require('mongoose');
 
 const config = require('./config');
 
-
 mongoose
   .connect(config.db_url)
   .then(() => console.log('\n <>--<>--<> connected to MongoDB <>--<>--<>\n'))
   .catch(err => console.log('Failed to connect to mongoDB'))
+
+const authController = require('./auth/authController');
 
 const server = express();
 
@@ -22,6 +23,8 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
 });
+
+server.use('/api/authentication', authController)
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n=== API up on port: ${port} ===\n`));
