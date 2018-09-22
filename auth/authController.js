8da57e16d4c59f86dd1a382a801b559469021538
Carlos Andrade <carlos.andrade86@outlook.com>
2018-09-21 22:49:51 -0400

    added username to the signup controller

--- auth/authController.js
+++ auth/authController.js
@@ -9,23 +9,28 @@ function tokenForUser(user) {
 }
 
 router.post('/signup', (req, res) => {
+    const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
+    
   
-    if (!email || !password) {
-      return res.status(422).send({ error: 'You must provide email and password'});
+    if (!username || !email || !password) {
+      return res.status(422).send({ error: 'You must provide username, email and password'});
     }
   
     User.findOne({ email: email }, function(err, existingUser) {
+      console.log(existingUser);
+      
       if (err) { return next(err); }
-  
+      
       if (existingUser) {
         return res.status(422).send({ error: 'Email is in use' });
-      }
+      }  
   
       const user = new User({
-        email: email,
-        password: password
+        username,
+        email,
+        password
       });
   
       user.save(function(err) {
