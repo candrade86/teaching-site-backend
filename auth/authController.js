f270f8cb4f18250f9fbc4b11286d62fc0b113612
Carlos Andrade <carlos.andrade86@outlook.com>
2018-09-23 20:44:15 -0400

    fixed error that was causing signup to fail

--- auth/authController.js
+++ auth/authController.js
@@ -5,7 +5,7 @@ const config = require('../config');
 
 function tokenForUser(user) {
   const timestamp = new Date().getTime();
-  return jwt.encode({ sub: user.id, iat: timestamp, username: username }, config.secret);
+  return jwt.encode({ sub: user.id, iat: timestamp, username: user.username }, config.secret);
 }
 
 router.post('/signup', (req, res) => {
