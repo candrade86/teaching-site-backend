f9ea9b7b909391166dcdb75105af3aff7d538171
Carlos Andrade <carlos.andrade86@outlook.com>
2018-09-28 20:46:36 -0400

    created an event schema for the scheduler

---
+++ events/eventModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  start: {
+    type: String,
+    required: true,
+  },
+  end: {
+    type: String,
+    required: true,
+  },
+
+});
+
+module.exports = mongoose.model("Event", eventSchema);
