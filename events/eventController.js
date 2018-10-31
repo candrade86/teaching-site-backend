1e313a89d980afd2033c007356c442d63c825cea
Carlos Andrade <carlos.andrade86@outlook.com>
2018-10-30 21:46:42 -0400

    created a route the returns an individual class based on the params passed to it.

--- events/eventController.js
+++ events/eventController.js
@@ -11,6 +11,18 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id',  (req, res) => {
+  const id = req.params;
+  
+  Events.find({id})
+    .then(events => {
+      res.status(200).json(events);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 router.post('/classes', (req, res) => {
   let title  = req.body.username;
   console.log('title', title)
